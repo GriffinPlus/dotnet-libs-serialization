3d40454b3377d50bc06b97e69f9912ebad497fbd
Sascha Falk <sascha@falk-online.eu>
2023-03-22 10:17:15 +0100

    Add custom serializer for GriffinPlus.Lib.Imaging.Color

---
+++ src/GriffinPlus.Lib.Serialization/GriffinPlus.Lib.Serialization/External Object Serializers/ExternalObjectSerializer_Color.cs
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/griffinplus/dotnet-libs-serialization)
+// The source code is licensed under the MIT license.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using GriffinPlus.Lib.Imaging;
+
+namespace GriffinPlus.Lib.Serialization
+{
+
+	/// <summary>
+	/// External object serializer for the <see cref="Color"/> struct.
+	/// </summary>
+	[ExternalObjectSerializer(1)]
+	public class ExternalObjectSerializer_Color : ExternalObjectSerializer<Color>
+	{
+		/// <summary>
+		/// Serializes the object.
+		/// </summary>
+		/// <param name="archive">Archive to serialize into.</param>
+		/// <param name="color">The color to serialize.</param>
+		/// <exception cref="VersionNotSupportedException">Serializer version is not supported.</exception>
+		public override void Serialize(SerializationArchive archive, Color color)
+		{
+			if (archive.Version == 1)
+			{
+				archive.Write(color.ScA);
+				archive.Write(color.ScR);
+				archive.Write(color.ScG);
+				archive.Write(color.ScB);
+				return;
+			}
+
+			throw new VersionNotSupportedException(archive);
+		}
+
+		/// <summary>
+		/// Deserializes an object.
+		/// </summary>
+		/// <param name="archive">Archive containing the serialized object.</param>
+		/// <returns>The deserialized object.</returns>
+		/// <exception cref="VersionNotSupportedException">Serializer version is not supported.</exception>
+		public override Color Deserialize(DeserializationArchive archive)
+		{
+			if (archive.Version == 1)
+			{
+				float a = archive.ReadSingle();
+				float r = archive.ReadSingle();
+				float g = archive.ReadSingle();
+				float b = archive.ReadSingle();
+				return Color.FromScRgb(a, r, g, b);
+			}
+
+			throw new VersionNotSupportedException(archive);
+		}
+	}
+
+}
